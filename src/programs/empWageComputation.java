99afaa42837af77ca4dfb23ef5fd674d8b23b370
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-17 03:05:58 +0530

    check employee attendance

--- src/programs/empWageComputation.java
+++ src/programs/empWageComputation.java
@@ -1,9 +1,22 @@
 package programs;
 import utility.Utilities;
+/**@author Bhavitha
+  *Create Date : 17th October 2020
+  *Functionality : Employee Wage Computations
+  */
 public class empWageComputation {
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
+
+        Utilities utilities=new Utilities();
+        //uc-1 check employee attendance
+        double attendance = utilities.CheckEmpAttendance();
+        if (attendance == 1)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+
     }
 
 }
--- src/utility/Utilities.java
+++ src/utility/Utilities.java
@@ -1,4 +1,18 @@
 package utility;
 
+/**@author Bhavitha
+ * Create Date   : 17th October 2020
+ * Functionality : This utility file contains logic of code for src.
+ */
 public class Utilities {
+    /*
+    @Functionality : checks if employee is present or absent
+     */
+    public int CheckEmpAttendance(){
+        int is_Present=1;
+        double randomCheck=Math.floor(Math.random()*10)%2;
+        if (is_Present == randomCheck)
+            return 1;
+        return 0;
+    }
 }
