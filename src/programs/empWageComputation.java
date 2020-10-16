f27497d34711bc75c29f7dae09a98338ed0cd979
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-17 02:16:08 +0530

    commit master

---
+++ src/programs/empWageComputation.java
@@ -0,0 +1,10 @@
+package programs;
+import utility.Utilities;
+public class empWageComputation {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation");
+    }
+
+}
+
